a930f2fb50adfb03cefd35b4e4aefadceff842ae
Duane <duanerichard47@hotmail.com>
2023-03-16 11:25:58 -0400

    incomplete column

--- server/models.py
+++ server/models.py
@@ -12,3 +12,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Message {self.body}, {self.username}, {self.created_at}, {self.updated_at}>'
